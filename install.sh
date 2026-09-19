70757ca504961e30c3764acbe3134a42374fbb95
Takabunbin <163474755+Takabunbin@users.noreply.github.com>
2026-09-19 16:36:32 +0800

    fix: use valid ShellCheck directives

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ fi
 old_real_curl=""
 old_real_wget=""
 if [[ -r "$CONF" ]]; then
-  # shellcheck disable=SC1090 -- CONF is the root-owned ghlane config.
+  # shellcheck disable=SC1090
   . "$CONF"
   old_real_curl="${REAL_CURL:-}"
   old_real_wget="${REAL_WGET:-}"
